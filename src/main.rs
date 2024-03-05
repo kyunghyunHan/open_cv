41c8d879ce8c8d16638de669739c57abead8a224
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-03-05 21:54:31 +0900

    feat:test

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod video_capture;
 mod video_feature;
 mod openc;
 fn main(){
-    video_capture::model::main();
+    face_detection::model::main().unwrap();
+    // video_capture::model::main();
     // video_capture::model::main();
     // openc::model::main();
     // video_capture::model::main();
