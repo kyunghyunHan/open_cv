4307ab6a05cbf883e928f380f3bb4f7e031eec22
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-03-25 17:37:59 +0900

    feat:video init

--- src/video/video.rs
+++ src/video/video.rs
@@ -1,13 +1,22 @@
-use opencv::{
-    core,
-    highgui,
-    imgproc,
-    objdetect,
-    types,
-    Result,
-    imgcodecs,
-};
-
+use opencv::{highgui, prelude::*, videoio, Result};
 pub fn main() -> Result<()> {
-    Ok(())
+	let window = "video capture";
+	highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
+	let mut cam = videoio::VideoCapture::from_file("girl.mp4", 0)?;
+	let opened = videoio::VideoCapture::is_opened(&cam)?;
+	if !opened {
+		panic!("Unable to open default camera!");
+	}
+	loop {
+		let mut frame = Mat::default();
+		cam.read(&mut frame)?;
+		if frame.size()?.width > 0 {
+			highgui::imshow(window, &frame)?;
+		}
+		let key = highgui::wait_key(10)?;
+		if key > 0 && key != 255 {
+			break;
+		}
+	}
+	Ok(())
 }
