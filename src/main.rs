01967789e7c2018d54a67649b13161e080b8e981
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-03-25 17:38:34 +0900

    feat:video Init

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@
 mod image;
 mod video;
 mod cam;
-
+mod snow;
 pub fn main()  {
 // image::image_face_object_detection::main().unwrap();
-image::image_cat_object_dection::main().unwrap();
+// image::image_cat_object_dection::main().unwrap();
 // image::img_eye_object_detection::main().unwrap();
+snow::main().unwrap();
 
 }
