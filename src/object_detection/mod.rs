54c710c40ac1771538ca00cf993a3fdacd758262
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-04-19 18:14:27 +0900

    feat:detect_eyes

--- src/object_detection/mod.rs
+++ src/object_detection/mod.rs
@@ -1,12 +1,12 @@
 use opencv::{
-    core::{self, no_array, MatTraitConst, Scalar},
+    core::{self, no_array, MatTrait, MatTraitConst, Scalar},
     highgui, imgcodecs, imgproc,
     objdetect::{self, CascadeClassifierTrait, CascadeClassifierTraitConst},
     Result,
 };
 
 pub fn main() -> Result<()> {
-    detect_face()?;
+    detect_eyes()?;
     Ok(())
 }
 fn template() -> Result<()> {
@@ -110,12 +110,85 @@ fn detect_face() -> Result<()> {
         core::Size_::default(),
         core::Size_::default(),
     )?;
-    let mut dst=src.clone();
-    for rc in faces{
-        imgproc::rectangle(&mut dst, rc,core::Scalar::from((255,0,255)), 2,imgproc::LINE_AA,0)?;
+    let mut dst = src.clone();
+    for rc in faces {
+        imgproc::rectangle(
+            &mut dst,
+            rc,
+            core::Scalar::from((255, 0, 255)),
+            2,
+            imgproc::LINE_AA,
+            0,
+        )?;
     }
     highgui::imshow("src", &dst)?;
     highgui::wait_key(0)?;
     highgui::destroy_all_windows()?;
+    Ok(())
+}
+
+fn detect_eyes() -> Result<()> {
+    let mut src = imgcodecs::imread("./img/face.jpg", imgcodecs::IMREAD_COLOR)?;
+    if src.empty() {
+        println!("{}", "error");
+        std::process::exit(0);
+    }
+
+    let xml = core::find_file_def("haarcascades/haarcascade_frontalface_alt.xml")?;
+    let eye_xml = core::find_file_def("haarcascades/haarcascade_eye_tree_eyeglasses.xml")?;
+
+    let mut face_classifier = objdetect::CascadeClassifier::new(&xml)?;
+    let mut eye_classifier = objdetect::CascadeClassifier::new(&eye_xml)?;
+
+    if face_classifier.empty()? || eye_classifier.empty()? {
+        println!("{}", "error");
+        std::process::exit(0);
+    }
+    let mut faces: core::Vector<core::Rect> = core::Vector::new();
+    face_classifier.detect_multi_scale(
+        &src,
+        &mut faces,
+        1.3,
+        1,
+        objdetect::CASCADE_SCALE_IMAGE,
+        core::Size_::default(),
+        core::Size_::default(),
+    )?;
+    println!("{}",faces.len());
+
+    // let mut dst = src.clone();
+    for face in faces {
+        imgproc::rectangle(
+            &mut src,
+            face,
+            core::Scalar::from((255, 0, 255)),
+            2,
+            imgproc::LINE_AA,
+            0,
+        )?;
+        let mut face_roi = src.roi_mut(face)?;
+       
+        let mut  eyes: core::Vector<core::Rect> = core::Vector::new();
+
+        eye_classifier.detect_multi_scale(
+            &face_roi,
+            &mut eyes,
+            1.1,
+            1,
+            objdetect::CASCADE_SCALE_IMAGE,
+            core::Size_::default(),
+            core::Size_::default(),
+        )?;
+        println!("{}",eyes.len());
+        for eye in eyes{
+          
+            let center= core::Point::from((eye.x+eye.width/2,eye.y+eye.height/2));
+            imgproc::circle(&mut face_roi, center, eye.width/2, core::Scalar::from((255,0,0)), 2, imgproc::LINE_AA, 0)?;
+        }
+    }
+    highgui::imshow("src", &src)?;
+    highgui::wait_key(0)?;
+    highgui::destroy_all_windows()?;
+
     Ok(())
 }
