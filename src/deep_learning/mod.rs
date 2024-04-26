1f2d722c16c3886391e1791c1329d3b5ed9e4ba7
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-04-26 23:41:19 +0900

    feat:machine learming

---
+++ src/deep_learning/mod.rs
@@ -0,0 +1,5 @@
+use opencv::Result;
+
+pub fn main() -> Result<()> {
+    Ok(())
+}
--- src/machine_learning/mod.rs
+++ src/machine_learning/mod.rs
@@ -1,4 +1,8 @@
-use opencv::{core, highgui, imgcodecs, imgproc, ml, Result};
+use opencv::{
+    core::{self, MatExprTraitConst},
+    highgui, imgcodecs, imgproc, ml, Result,
+};
+use std::sync::{Arc, Mutex};
 /*머신러닝
 학습 = train
 결과 예측=>predict,inference
@@ -7,7 +11,10 @@ pub fn main() -> Result<()> {
     Ok(())
 }
 fn knn_plane() -> Result<()> {
-    let img = core::Mat::zeros(500, 500, core::CV_8UC3)?;
+    let img = Arc::new(Mutex::new(
+        core::Mat::zeros(500, 500, core::CV_8UC3)?.to_mat()?,
+    ));
+    let img_clone = Arc::clone(&img);
     let knn = ml::KNearest::create()?;
     let mut k_value = 1;
     highgui::named_window("knn", 0)?;
@@ -16,12 +23,19 @@ fn knn_plane() -> Result<()> {
         "knn",
         Some(&mut k_value),
         5,
-        Some(Box::new({ |x| {
-
-
-            
-        } })),
+        Some(Box::new({
+            move |x| {
+                let img_guard = img_clone.lock().unwrap();
+            }
+        })),
     )?;
+    let num = 30;
+    let mut rn = core::Mat::new_rows_cols_with_default(num, 2, core::CV_32SC1, core::Scalar::default())?;
 
+    core::randn(&mut rn, &0., &50.)?;
+
+    for i in 0..num{
+     //addPoint
+    }
     Ok(())
 }
