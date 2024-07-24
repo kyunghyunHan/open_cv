b6a2a1e353a4a1ddff9c183814b2f43c9e55188c
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-07-24 18:20:21 +0900

    feat:setting

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod color_op;
 mod contrast;
 mod corners;
 mod cv_dl;
+mod deep_learning;
 mod drawing;
 mod edges;
 mod filter;
@@ -17,30 +18,37 @@ mod keyboard;
 mod keypoint;
 mod labeling;
 mod machine_learning;
+mod mat_op;
 mod matching;
 mod module;
 mod mouse_event;
 mod object_detection;
 mod perspective;
 mod qr_code;
+mod resnet_image;
+mod scanner;
 mod snow;
 mod ssd_face_detector;
 mod storage;
 mod torch;
+mod tracker;
 mod utils;
 mod video;
-mod deep_learning;
+mod web_cam_streaming_server;
+mod window_capture;
 mod yolo;
 mod yolo_image;
-mod resnet_image;
-mod web_cam_streaming_server;
-mod tracker;
-mod window_capture;
-mod scanner;
-mod mat_op;
 pub fn main() {
-    // video::main().unwrap();
-  
-mat_op::main().unwrap();
-  
+    /*tracker */
+    // tracker::main().unwrap();
+    
+    /* */
+    // ssd_face_detector::main().unwrap();
+
+    /*drawing */
+    // drawing::main().unwrap();
+    // mat_op::main().unwrap();
+
+    /*google_net*/
+    google_net::main().unwrap();
 }
