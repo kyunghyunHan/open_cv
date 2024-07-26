bb597aa4a8c1e028209abadf2ce48e2bdd329083
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-07-26 16:46:45 +0900

    delete python

--- py/p_2_2.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import sys
-
-img = cv.imread("./img/face.jpg")
-if img is None:
-    sys.exit("파일을 찾을수 없습니다")
-cv.imshow("image Display",img)
-cv.waitKey()
-cv.destroyAllWindows()
