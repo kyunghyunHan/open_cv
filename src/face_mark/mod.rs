acc67712a9272bc3ed2d9f1e4a83f66e9ddd5bba
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-07-28 22:25:06 +0900

    feat:number

--- src/face_mark/mod.rs
+++ src/face_mark/mod.rs
@@ -2,7 +2,9 @@ use opencv::{
     core::{Point, Point2f, Rect, Scalar, Size_, Vector},
     face::FacemarkLBF,
     highgui,
-    imgproc::{circle, cvt_color, polylines, COLOR_BGR2GRAY, LINE_8},
+    imgproc::{
+        circle, cvt_color, polylines, put_text, COLOR_BGR2GRAY, FONT_HERSHEY_SIMPLEX, LINE_8,
+    },
     objdetect::CascadeClassifier,
     prelude::*,
     videoio, Result,
@@ -15,7 +17,7 @@ pub fn main() -> Result<()> {
     facemark.load_model("./dataset/lbfmodel.yaml").unwrap();
     let window = "video capture";
     highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
-    let mut cam: videoio::VideoCapture = videoio::VideoCapture::from_file("./video/smile1.mov", 0)?;
+    let mut cam = videoio::VideoCapture::from_file("./video/smile1.mov", 0)?;
     let opened = videoio::VideoCapture::is_opened(&cam)?;
     if !opened {
         panic!("Unable to open video file!");
@@ -33,14 +35,14 @@ pub fn main() -> Result<()> {
             .detect_multi_scale(
                 &mut gray,
                 &mut faces,
-                1.2,
+                1.1,
                 3,
                 0,
                 Size_::default(),
                 Size_::default(),
             )
             .unwrap();
-        
+
         if faces.len() > 0 {
             println!("Faces detected: {}", faces.len());
         }
@@ -75,7 +77,8 @@ fn draw_polyline(
     let mut points: Vector<Point> = Vector::new();
     for i in start..=end {
         let point = landmarks.get(i as usize).unwrap();
-        points.push(Point::new(point.x.round() as i32, point.y.round() as i32)); // 좌표를 반올림하여 정수형으로 변환
+        points.push(Point::new(point.x.round() as i32, point.y.round() as i32));
+        // 좌표를 반올림하여 정수형으로 변환
     }
 
     polylines(
@@ -93,17 +96,17 @@ fn draw_polyline(
 fn draw_landmarks(im: &mut Mat, landmarks: &Vector<Point2f>) -> opencv::Result<()> {
     if landmarks.len() != 68 {
         println!("Drawing landmarks with 68 points");
-        draw_polyline(im, &landmarks, 0, 16, false)?;   // Jaw line
-        draw_polyline(im, &landmarks, 17, 21, false)?;  // Left eyebrow
-        draw_polyline(im, &landmarks, 22, 26, false)?;  // Right eyebrow
-        draw_polyline(im, &landmarks, 27, 30, false)?;  // Nose bridge
-        draw_polyline(im, &landmarks, 30, 35, true)?;   // Lower nose
-        draw_polyline(im, &landmarks, 36, 41, true)?;   // Left eye
-        draw_polyline(im, &landmarks, 42, 47, true)?;   // Right eye
-        draw_polyline(im, &landmarks, 48, 59, true)?;   // Outer lip
-        draw_polyline(im, &landmarks, 60, 67, true)?;   // Inner lip
+        draw_polyline(im, &landmarks, 0, 16, false)?; // Jaw line
+        draw_polyline(im, &landmarks, 17, 21, false)?; // Left eyebrow
+        draw_polyline(im, &landmarks, 22, 26, false)?; // Right eyebrow
+        draw_polyline(im, &landmarks, 27, 30, false)?; // Nose bridge
+        draw_polyline(im, &landmarks, 30, 35, true)?; // Lower nose
+        draw_polyline(im, &landmarks, 36, 41, true)?; // Left eye
+        draw_polyline(im, &landmarks, 42, 47, true)?; // Right eye
+        draw_polyline(im, &landmarks, 48, 59, true)?; // Outer lip
+        draw_polyline(im, &landmarks, 60, 67, true)?; // Inner lip
     } else {
-        for point in landmarks {
+        for (i, point) in landmarks.iter().enumerate() {
             circle(
                 im,
                 Point::new(point.x.round() as i32, point.y.round() as i32), // 좌표를 반올림하여 정수형으로 변환
@@ -112,6 +115,17 @@ fn draw_landmarks(im: &mut Mat, landmarks: &Vector<Point2f>) -> opencv::Result<(
                 0,
                 opencv::imgproc::LINE_8,
                 0,
+            )?;
+            put_text(
+                im,
+                &i.to_string(),
+                Point::new(point.x.round() as i32, point.y.round() as i32),
+                FONT_HERSHEY_SIMPLEX,
+                0.4,
+                Scalar::from((255, 0, 0)),
+                1,
+                LINE_8,
+                false,
             )?;
         }
     }
