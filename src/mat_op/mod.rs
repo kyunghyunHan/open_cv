39e90c620b45e1e120c5df8c11a51b2986ef4a9c
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-08-08 14:57:07 +0900

    feat:mat_op

--- src/mat_op/mod.rs
+++ src/mat_op/mod.rs
@@ -13,17 +13,17 @@ pub fn main() -> Result<()> {
     }
     let rect = Rect::new(150, 150, 150, 150);
 
-    let img2 = Mat::roi(&img1, rect).unwrap().try_clone().unwrap();
+    let img2 = Mat::roi(&img1, rect)?;
     let img3 = Mat::roi(&img1, rect)?.clone_pointee();
 
   
     
-    let mut img2 = Mat::default();
+    let mut img2_not = Mat::default();
 
-    bitwise_not(&img1, &mut img2, &no_array()).unwrap();
+    bitwise_not(&img2, &mut img2_not, &no_array()).unwrap();
 
     imshow("img1", &img1)?;
-    imshow("img2", &img2)?;
+    imshow("img2", &img2_not)?;
     imshow("img3", &img3)?;
     wait_key(0)?;
 
