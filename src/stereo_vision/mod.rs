7d8159028174cc1d0602bceac4bce3c31f9cad97
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-08-12 11:28:35 +0900

    feat:test

--- src/stereo_vision/mod.rs
+++ src/stereo_vision/mod.rs
@@ -1,62 +1,62 @@
 use opencv::{
     calib3d::StereoSGBM,
-    core::{no_array, Mat, MatTraitConst, Scalar, CV_8U, CV_8UC1},
-    highgui::{create_trackbar, imshow, wait_key},
+    core::{Mat, CV_8U},
+    highgui::{create_trackbar, imshow, wait_key, named_window, WINDOW_AUTOSIZE},
     imgcodecs::{imread, ImreadModes::IMREAD_GRAYSCALE},
-    imgproc::{self, apply_color_map, COLORMAP_JET},
-    prelude::{MatExprTraitConst, MatTrait, StereoMatcherTrait},
+    imgproc::apply_color_map,
+    prelude::{MatTraitConst, StereoMatcherTrait},
     Result,
 };
 use std::sync::Arc;
 use std::sync::Mutex;
-/*깊이추정
-disparity =x−x′=Bf/Z
 
+const DISPARITY_WINDOW: &str = "disparity";
 
-
-*/
-//차이 계산
-const disparity_window: &str = "disparity";
-const block_size: i32 = 5;
 fn calculate_difference() -> Result<()> {
+    let img_l = imread("./img/bike0.png", 0)?;
+    let img_r = imread("./img/im1.png", 0)?;
+
+    let img_l = Arc::new(Mutex::new(img_l));
+    let img_r = Arc::new(Mutex::new(img_r));
+
+    named_window(DISPARITY_WINDOW, WINDOW_AUTOSIZE)?;
+
+    let img_l_clone = Arc::clone(&img_l);
+    let img_r_clone = Arc::clone(&img_r);
     let mut num_disparity = 8;
-    let dsip = Mat::default();
-    let disparity = Mat::default();
 
-    let img: Arc<Mutex<Mat>> = Arc::new(Mutex::new(imread("./img/face1.jpeg", 0)?));
-
-    let img_clone = Arc::clone(&img);
     create_trackbar(
         "numDisparities",
-        &disparity_window,
-        Some(&mut num_disparity),
+        DISPARITY_WINDOW,
+        None,
         18,
-        Some(Box::new(move |num_disparity| {
-            tarkbar1(num_disparity, Arc::clone(&img_clone)).unwrap();
+        Some(Box::new(move |pos| {
+            trackbar1(pos, Arc::clone(&img_l_clone), Arc::clone(&img_r_clone)).unwrap();
         })),
     )?;
+
     wait_key(0)?;
     Ok(())
 }
-fn tarkbar1(num_disparity: i32, img_clone: Arc<Mutex<Mat>>) -> Result<()> {
-    // StereoSGBM::set_num_disparities(&mut self, num_disparities)
+
+fn trackbar1(num_disparity: i32, img_l: Arc<Mutex<Mat>>, img_r: Arc<Mutex<Mat>>) -> Result<()> {
+
     let mut stereo = StereoSGBM::create_def()?;
     stereo.set_num_disparities(num_disparity * 16)?;
-    let mut dsip = Mat::default();
+    let mut disp = Mat::default();
     let mut disparity = Mat::default();
 
-    let img_l = Mat::default();
-    let img_r = Mat::default();
-    stereo.compute(&img_l, &img_r, &mut dsip)?;
-    dsip.convert_to(&mut disparity, CV_8U, 0., 0.)?;
-    let mut disparity2 = Mat::default();
-    apply_color_map(&disparity, &mut disparity2, COLORMAP_JET)?;
-    // let mut img_guard = img_clone.lock().unwrap();
-    // img_guard.set_to(&Scalar::from(pos * 16), &no_array())?;
-    imshow(&disparity_window, &disparity2)?;
+    let img_l = img_l.lock().unwrap();
+    let img_r = img_r.lock().unwrap();
+    stereo.compute(&*img_l, &*img_r, &mut disp)?;
+    disp.convert_to(&mut disparity, CV_8U, 0., 0.)?;
+    let mut disparity_color = Mat::default();
+    apply_color_map(&disparity, &mut disparity_color, opencv::imgproc::COLORMAP_JET)?;
 
+    imshow(DISPARITY_WINDOW, &disparity_color)?;
     Ok(())
 }
+
 pub fn main() -> Result<()> {
     calculate_difference()?;
     Ok(())
