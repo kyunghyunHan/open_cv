dd16f7ee64940758507c0876e1e4f8068e4e868d
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-08-29 17:54:18 +0900

    feat:time out

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,5 @@
 use opencv::{
-    core::{MatTrait, MatTraitConst, Scalar},
+    core::{Mat, MatTrait, MatTraitConst, Scalar, TickMeter, TickMeterTrait, TickMeterTraitConst},
     highgui::{destroy_all_windows, imshow, wait_key},
     imgcodecs::{imread, IMREAD_COLOR, IMREAD_GRAYSCALE},
     Result,
@@ -21,7 +21,7 @@ fn mask_set_to() -> Result<()> {
     Ok(())
 }
 fn mask_copy_to() -> Result<()> {
-    let  src = imread("./img/airplane.bmp", IMREAD_COLOR)?;
+    let src = imread("./img/airplane.bmp", IMREAD_COLOR)?;
     let mask = imread("./img/mask_plane.bmp", IMREAD_GRAYSCALE)?;
     let mut dst = imread("./img/field.bmp", IMREAD_COLOR)?;
     if src.empty() || mask.empty() || dst.empty() {
@@ -34,8 +34,28 @@ fn mask_copy_to() -> Result<()> {
 
     Ok(())
 }
+
+fn time_inverse() -> Result<()> {
+    let mut src = imread("./img/airplane.bmp", IMREAD_GRAYSCALE)?;
+    if src.empty() {
+        panic!("image load faild")
+    }
+
+    let mut dst = unsafe { Mat::new_rows_cols(src.rows(), src.cols(), src.typ()) }?;
+    let mut tm: TickMeter = TickMeter::default()?;
+    tm.start()?;
+    for j in 0..src.rows() {
+        for i in 0..src.cols() {
+            *dst.at_2d_mut::<u8>(j, i)? = 255 - *src.at_2d_mut::<u8>(j, i)?;
+        }
+    }
+    tm.stop()?;
+    println!("image inverse took{:?}",tm.get_time_milli());
+    Ok(())
+}
 pub fn main() -> Result<()> {
     // mask_set_to()?;
-    mask_copy_to()?;
+    // mask_copy_to()?;
+    time_inverse()?;
     Ok(())
 }
